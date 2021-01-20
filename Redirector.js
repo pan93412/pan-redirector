ac4d43d6f44c6ddcdf5faddf83f436c9a63a4053
Yi-Jyun Pan <pan93412@gmail.com>
2021-01-20 12:30:30 +0800

    feat(redirector): added Redirector.js

---
+++ Redirector.js
@@ -0,0 +1,39 @@
+/**
+ * Redirector to the dynamic URLs.
+ */
+
+class Redirector {
+    constructor(hostTable, pathTable) {
+        // HostTable
+        this.ht = hostTable;
+        // PathTable
+        this.pt = pathTable;
+    }
+
+    hasHost(hostId) {
+        return (hostId in this.ht);
+    }
+
+    setHost(hostId) {
+        this.url = new URL(this.ht[hostId]);
+        return this;
+    }
+
+    hasPath(pathId) {
+        return (pathId in this.pt);
+    }
+
+    setPath(pathId) {
+        this.url.pathname = this.pt[pathId];
+        return this;
+    }
+    
+    build() {
+        return this.url.toString();
+    }
+
+    redirect() {
+        location.href = this.build();
+        return;
+    }
+}
