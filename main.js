b947f6388d6d86a04616d7f2bfeb4b828c8e558e
Yi-Jyun Pan <pan93412@gmail.com>
2021-01-20 12:30:52 +0800

    feat(main): added main.js

---
+++ main.js
@@ -0,0 +1,22 @@
+const redirect = new Redirector(hosts, path);
+const extractor = new Extractor();
+const manualRedirect = document.getElementById('redirect');
+
+function main() {
+    let _host = "", _path = "";
+
+    if (!extractor.has('host') || !extractor.has('path')) {
+        exception(MAIN__UNDEFINED_PARAMS + MAIN__URL_MALFORMALED);
+    }
+    
+    _host = extractor.get('host');
+    _path = extractor.get('path');
+    
+    if (!redirect.hasHost(_host) || !redirect.hasPath(_path)) {
+        exception(MAIN__PARAMS_INVAILD + MAIN__URL_MALFORMALED);
+    }
+    
+    redirect.setHost(_host).setPath(_path);
+    manualRedirect.href = redirect.build();
+    //redirect.redirect();
+}
