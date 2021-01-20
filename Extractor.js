dafe90d39e5cc1f8774d3539c9a3a2bfcf497465
Yi-Jyun Pan <pan93412@gmail.com>
2021-01-20 12:30:20 +0800

    feat(extractor): added Extractor.js

---
+++ Extractor.js
@@ -0,0 +1,19 @@
+class Extractor {
+    constructor() {
+        this.url = new URL(location.href);
+    }
+
+    /**
+     * @param {string} id
+     */
+    get(id) {
+        return this.url.searchParams.get(id).toString();
+    }
+
+    /**
+     * @param {string} id 
+     */
+    has(id) {
+        return this.url.searchParams.has(id);
+    }
+}
